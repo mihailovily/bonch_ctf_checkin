c9637830cc1870b72e97838b890b5363dff97b3a
Ilya <mihailovily@tuta.io>
2024-05-22 15:28:24 +0300

    2.0

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from telebot import types
 import telebot
 import sqlite3
+import random
+from datetime import datetime
 
 
 token = open("tokens/telegram.txt").readline()
@@ -46,7 +48,7 @@ def check_code(message):
         "SELECT is_active FROM ActiveCheckins WHERE code IS '{code}'".format(code=flag)
     )
     result = cursor.fetchall()
-    print(result[0][0])
+    # print(result[0][0])
     bot.reply_to(message, "Проверка кода...")
     if result != []:
         if result[0][0] == 1:
@@ -70,13 +72,13 @@ def checkin_user(message):
     )
     users = cursor.fetchall()
     if users == []:
-        print("nouser globally")
+        # print("nouser globally")
         cursor.execute(
             "INSERT INTO Users (tgid, username, name, visits) VALUES (?, ?, ?, ?)",
             (str(usr_id), str(usr_username), str(usr_first), 1),
         )
     else:
-        print("exists")
+        # print("exists")
         cursor.execute(
             "SELECT visits FROM Users WHERE tgid IS '{tgid}'".format(tgid=str(usr_id))
         )
@@ -85,29 +87,38 @@ def checkin_user(message):
             "UPDATE Users SET visits = ? WHERE tgid = ?", (user_visits + 1, str(usr_id))
         )
     # локальная отметка
+    # сопоставляем код и таблицу
+    cursor.execute(
+        "SELECT data FROM ActiveCheckins WHERE code IS '{tgid}'".format(
+            tgid=str(message.text)
+        )
+    )
+    local_table_name = cursor.fetchall()[0][0]
+
     cursor.execute(
         "SELECT * FROM '{table}' WHERE tgid IS '{tgid}'".format(
-            tgid=str(usr_id), table=message.text
+            tgid=str(usr_id), table=str(local_table_name)
         )
     )
     users = cursor.fetchall()
     if users == []:
-        print("nouser locally")
+        # print("nouser locally")
         cursor.execute(
             "INSERT INTO '{table}' (tgid, username, name) VALUES ('{tgid}', '{username}', '{name}')".format(
-                table=str(message.text),
+                table=str(local_table_name),
                 tgid=str(usr_id),
                 username=str(usr_username),
                 name=str(usr_first),
             )
         )
-        
+
     else:
-        print("exists locally")
+        pass
+        # print("exists locally")
 
     connection.commit()
     connection.close()
-    bot.reply_to(message, "Отлично, записали на занятие")
+    bot.reply_to(message, "Отлично, записал на занятие")
 
 
 def is_user_exists(tgid, dbname):
@@ -117,7 +128,16 @@ def is_user_exists(tgid, dbname):
 @bot.message_handler(commands=["visits"])
 @bot.message_handler(regexp="Мои посещения")
 def my_visits(message):
-    bot.reply_to(message, "В разработке")
+    connection = sqlite3.connect("my_database.db")
+    cursor = connection.cursor()
+    usr_id = message.from_user.id
+    cursor.execute(
+        "SELECT visits FROM Users WHERE tgid IS '{tgid}'".format(tgid=str(usr_id))
+    )
+    bot.reply_to(
+        message,
+        "Количество твоих посещений: {visits}".format(visits=cursor.fetchall()[0][0]),
+    )
 
 
 @bot.message_handler(commands=["admin"])
@@ -134,17 +154,59 @@ def admin_check(message):
 
 def admin_razvilka(message):
     if message.text == "1":
-        bot.reply_to(message, "Задавай код отметки и дату через пробел")
+        bot.reply_to(message, "Подтверди намерения")
         bot.register_next_step_handler(message, start_check)
     elif message.text == "2":
-        bot.reply_to(message, "Занятие с каким кодом необходимо завершить?")
+        connection = sqlite3.connect("my_database.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT code FROM ActiveCheckins WHERE is_active IS 1")
+        to_answer = "Занятие с каким кодом необходимо завершить?\n" + str(
+            cursor.fetchall()
+        )
+        bot.reply_to(message, to_answer)
         bot.register_next_step_handler(message, finish_check)
 
 
 def start_check(message):
-    msg = message.text.split()
-    date_check = msg[1]
-    code_check = msg[0]
+    if str(message.text) in "Yy":
+        date_check = datetime.today().strftime("%d.%m.%Y")
+        code_check = gen_auth_code()
+        usr_id = str(message.from_user.id)
+        connection = sqlite3.connect("my_database.db")
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT * FROM ActiveCheckins WHERE code IS '{codd}'".format(
+                codd=str(code_check)
+            )
+        )
+        if cursor.fetchall() == []:
+            cursor.execute(
+                "INSERT INTO ActiveCheckins (data, code, is_active) VALUES (?, ?, ?)",
+                (str(date_check), str(code_check), 1),
+            )
+            cursor.execute(
+                """CREATE TABLE IF NOT EXISTS '{table}' (id INTEGER PRIMARY KEY, tgid TEXT, username TEXT, name TEXT)""".format(
+                    table=str(date_check)
+                )
+            )
+            bot.send_message(
+                usr_id, "Успешно, код отметки {otmetka}".format(otmetka=code_check)
+            )
+        else:
+            bot.send_message(usr_id, "Занято")
+        connection.commit()
+        connection.close()
+
+
+def gen_auth_code():
+    s = ""
+    for i in range(5):
+        s += random.choice("QWERTYUIPASDFGHJKLZXCVBNM123456789")
+    return s
+
+
+def finish_check(message):
+    code_check = message.text
     usr_id = str(message.from_user.id)
     connection = sqlite3.connect("my_database.db")
     cursor = connection.cursor()
@@ -153,25 +215,33 @@ def start_check(message):
             codd=str(code_check)
         )
     )
-    if cursor.fetchall() == []:
+    if cursor.fetchall() != []:
         cursor.execute(
-            "INSERT INTO ActiveCheckins (data, code, is_active) VALUES (?, ?, ?)",
-            (str(date_check), str(code_check), 1),
+            "UPDATE ActiveCheckins SET is_active = ? WHERE code = ?",
+            (0, str(code_check)),
         )
         cursor.execute(
-            """CREATE TABLE IF NOT EXISTS '{table}' (id INTEGER PRIMARY KEY, tgid TEXT, username TEXT, name TEXT)""".format(
-                table=str(code_check)
+            "SELECT data FROM ActiveCheckins WHERE code IS '{tgid}'".format(
+                tgid=str(code_check)
             )
         )
-        bot.send_message(usr_id, "Успешно")
+        local_table_name = cursor.fetchall()[0][0]
+        cursor.execute(
+                    "SELECT * FROM '{tablitsa}'".format(
+                        tablitsa=str(local_table_name)
+                    )
+                )
+        users_finally = cursor.fetchall()
+        otchet = ''
+        for k in users_finally:
+            otchet += '{n}. id: {id}, username: {username}, name: {name}\n'.format(n=k[0], id=k[1], username=k[2], name=k[3])
+            
+        bot.send_message(usr_id, "Занятие {date} с кодом {code} успешно завершено. Всего отметившихся: {visited}".format(date=str(local_table_name), code=code_check, visited=str(len(users_finally))))
+        bot.send_message(usr_id, otchet)    
     else:
-        bot.send_message(usr_id, "Занято")
+        bot.send_message(usr_id, "Такого не найдено")
     connection.commit()
     connection.close()
-
-
-def finish_check(message):
-    pass
 
 
 # если сообщение не распознано
